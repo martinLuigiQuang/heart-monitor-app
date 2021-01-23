1d941b6f7a297b7fed25ed808559e9c51ae46380
Martin Nguyen <martin.nguyen.20289@gmail.com>
2021-01-23 14:07:05 -0500

    first commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header>
+            <h1>Heart Monitor</h1>
+            <nav>
+                <ul>
+                    
+                </ul>
+            </nav>
+        </header>
+    );
+}
